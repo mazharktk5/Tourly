3347363d4e206448d6c41be4988f2e01c59a626a
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-05-14 11:58:33 +0500

    updated

--- booknow.php
+++ booknow.php
@@ -7,37 +7,37 @@ $checkin = $_POST['checkin'];
 $checkout = $_POST['checkout'];
 $people = $_POST['people'];
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "tourly";
 
-$conn = new mysqli("localhost", "root", "", "tourly");
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-if($conn){
-    echo "Connection successful";
-}
+if (!$conn) {
+    echo "Connection not successful";
+} else {
+    echo "connection established";
 
 
 $sql = "INSERT INTO booking(name, email, destination, checkin, checkout, people)
         VALUES ('$name', '$email', '$destination', '$checkin', '$checkout', $people)";
 
-if ($conn->query($sql) === TRUE){
-    echo "<h1>Booking Confirmation</h1>";
-    echo "<p>Thank you, $name, for booking your trip!</p>";
-    echo "<p>Details:</p>";
-    echo "<ul>";
-    echo "<li>Email: $email</li>";
-    echo "<li>Destination: $destination</li>";
-    echo "<li>Check-in Date: $checkin</li>";
-    echo "<li>Check-out Date: $checkout</li>";
-    echo "<li>Number of People: $people</li>";
-    echo "</ul>";
-    echo "<p>We will send a confirmation email to $email shortly.</p>";
-    echo "<p>Thank you for choosing our service!</p>";
-    echo "<p><a href='./index.html'>Back to Home</a></p>";
-} else {
-    die("Connection failed: " . mysqli_connect_error());
+
+//     echo "<h1>Booking Confirmation</h1>";
+//     echo "<p>Thank you, $name, for booking your trip!</p>";
+//     echo "<p>Details:</p>";
+//     echo "<ul>";
+//     echo "<li>Email: $email</li>";
+//     echo "<li>Destination: $destination</li>";
+//     echo "<li>Check-in Date: $checkin</li>";
+//     echo "<li>Check-out Date: $checkout</li>";
+//     echo "<li>Number of People: $people</li>";
+//     echo "</ul>";
+//     echo "<p>We will send a confirmation email to $email shortly.</p>";
+//     echo "<p>Thank you for choosing our service!</p>";
+//     echo "<p><a href='./index.html'>Back to Home</a></p>";
+
 }
 
 mysqli_close($conn);
